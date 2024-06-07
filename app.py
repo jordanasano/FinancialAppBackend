96bfa8146c2b00245a86a8e51042ea8151067502
Jordan Asano <jordanasano@yahoo.com>
2024-06-07 11:47:45 -0700

    Set up dependencies, gitignore, and wrote the first endpoint to calculate your net takehome pay per month

---
+++ app.py
@@ -0,0 +1,225 @@
+from flask import Flask
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+import requests
+import json
+from datetime import date
+
+app = Flask(__name__)
+
+load_dotenv()
+
+# Initialize Gemini
+genai.configure(api_key=os.environ["API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+@app.post('/monthlyTakeHomeForCA/<int:annualIncome>')
+def getMonthlyTakeHomeForCA(annualIncome):
+	taxApi = "https://paycheck-calculator.adp.com/api/pcc/v2/calculations"
+
+	#region Private
+	data = {
+	"calculationTypeCode": {
+		"code": "GROSS_TO_NET"
+	},
+	"statutoryPolicyInputs": [
+		{
+			"id": "w4Form2020Indicator",
+			"name": "w4Form2020Indicator",
+			"value": True,
+			"type": "boolean",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "nonResidentAlienIndicator",
+			"name": "nonResidentAlienIndicator",
+			"value": False,
+			"type": "boolean",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "filingStatus",
+			"name": "filingStatus",
+			"value": "SINGLE",
+			"type": "string",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "useHigherRateIndicator",
+			"name": "useHigherRateIndicator",
+			"value": False,
+			"type": "boolean",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "claimedDependentsAmount",
+			"name": "claimedDependentsAmount",
+			"value": 0,
+			"type": "amount",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "otherIncomeAmount",
+			"name": "otherIncomeAmount",
+			"value": 0,
+			"type": "amount",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "deductionsAmount",
+			"name": "deductionsAmount",
+			"value": 0,
+			"type": "amount",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "additionalWithholdingAmount",
+			"name": "additionalWithholdingAmount",
+			"value": 0,
+			"type": "amount",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "federalTax",
+			"name": "withholdingStatus",
+			"value": "SUBJECT",
+			"type": "string",
+			"templateID": "e01a6863-4fc7-4c2a-ac8c-f8d896c6fba2"
+		},
+		{
+			"id": "medicare",
+			"name": "withholdingStatus",
+			"value": "SUBJECT",
+			"type": "string",
+			"templateID": "8b2a4033-22a2-452b-b2fb-33606c891238"
+		},
+		{
+			"id": "socialSecurity",
+			"name": "withholdingStatus",
+			"value": "SUBJECT",
+			"type": "string",
+			"templateID": "c560a4be-f8a7-4fdb-a0e9-61920ec81c92"
+		},
+		{
+			"id": "filingStatus",
+			"name": "filingStatus",
+			"value": "SINGLE",
+			"type": "string",
+			"templateID": "591baa3b-5d81-40fa-aac1-609aac5ab758"
+		},
+		{
+			"id": "numberOfAllowances",
+			"name": "numberOfAllowances",
+			"value": 0,
+			"type": "number",
+			"templateID": "591baa3b-5d81-40fa-aac1-609aac5ab758"
+		},
+		{
+			"id": "numberOfAdditionalAllowances",
+			"name": "numberOfAdditionalAllowances",
+			"value": 0,
+			"type": "number",
+			"templateID": "591baa3b-5d81-40fa-aac1-609aac5ab758"
+		},
+		{
+			"id": "additionalWithholdingAmount",
+			"name": "additionalWithholdingAmount",
+			"value": 0,
+			"type": "amount",
+			"templateID": "591baa3b-5d81-40fa-aac1-609aac5ab758"
+		},
+		{
+			"id": "withholdingStatus",
+			"name": "withholdingStatus",
+			"value": "SUBJECT",
+			"type": "string",
+			"templateID": "591baa3b-5d81-40fa-aac1-609aac5ab758"
+		},
+		{
+			"id": "withholdingStatus_SDI",
+			"name": "withholdingStatus",
+			"value": "SUBJECT",
+			"type": "string",
+			"templateID": "1c569bfa-33bd-4107-8a49-27ccb3397a5e"
+		}
+	],
+	"jurisdictions": {
+		"workedInJurisdictions": [
+			{
+				"jurisdictionID": "dea07e6d-9432-4f65-958b-25f09e18117e",
+				"jurisdictionCode": {
+					"name": "United States Federal",
+					"code": "US"
+				},
+				"jurisdictionLevelCode": {
+					"code": "FEDERAL"
+				}
+			},
+			{
+				"jurisdictionID": "aa4c44ae-c32c-4a5a-a59f-fcbcfe564b1c",
+				"jurisdictionCode": {
+					"name": "California",
+					"code": "CA"
+				},
+				"jurisdictionLevelCode": {
+					"code": "STATE"
+				}
+			}
+		],
+		"livedInJurisdictions": [
+			{
+				"jurisdictionID": "dea07e6d-9432-4f65-958b-25f09e18117e",
+				"jurisdictionCode": {
+					"name": "United States Federal",
+					"code": "US"
+				},
+				"jurisdictionLevelCode": {
+					"code": "FEDERAL"
+				}
+			},
+			{
+				"jurisdictionID": "aa4c44ae-c32c-4a5a-a59f-fcbcfe564b1c",
+				"jurisdictionCode": {
+					"name": "California",
+					"code": "CA"
+				},
+				"jurisdictionLevelCode": {
+					"code": "STATE"
+				}
+			}
+		]
+	},
+	"payDate": f"{date.today()}",
+	"payFrequencyCode": {
+		"code": "MONTHLY"
+	},
+	"businessPolicies": [
+		{
+			"id": "salary-1",
+			"alias": "salary",
+			"label": "SALARY",
+			"inputs": [
+				{
+					"name": "appliedPayPeriodAmount",
+					"value": annualIncome,
+					"type": "amount"
+				}
+			]
+		}
+	],
+	"additionalEarnings": {
+		"payLines": []
+	},
+	"deductions": []
+	}
+	#endregion
+
+	try:
+		response = requests.post(taxApi, json = data)
+		monthlyTakeHomePay = json.loads(response.text)["net"]["amount"]
+		return { "monthlyTakeHomePay": monthlyTakeHomePay }
+	except Exception as error:
+		return f"Error hit. {error}"
+
+
