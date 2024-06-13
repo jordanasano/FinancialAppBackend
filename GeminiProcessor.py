0418de1e770414e041020f245fcab288acf09e2d
Jordan Asano <jordanasano@yahoo.com>
2024-06-13 11:22:25 -0700

    Updated gemini string processing to add formatting to the output html

--- GeminiProcessor.py
+++ GeminiProcessor.py
@@ -4,6 +4,9 @@ class GeminiProcessor:
 		textList = text.split("\n")
 
 		for index, value in enumerate(textList):
+			while "**" in value:
+				value = value.replace("**", "<strong>", 1)
+				value = value.replace("**", "</strong>", 1)
 			textList[index] = f"<p>{value}</p>"
 		
 		htmlString = f"<div>{''.join(textList)}</div>"
